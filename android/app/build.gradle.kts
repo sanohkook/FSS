55529ad5c76efe3308f881759d0bf1d56ff56da8
bit4man <bit4man@gmarket.com>
2026-09-09 08:55:01 +0900

    release v2.5 (versionCode 7)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "kr.co.fss.yeyak"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "2.4"
+        versionCode = 7
+        versionName = "2.5"
     }
 
     buildFeatures {
