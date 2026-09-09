c6d8b79a3170ba2317cacd38c7d65fb7f4a99ee2
bit4man <bit4man@gmarket.com>
2026-09-09 12:52:00 +0900

    release v2.13 (versionCode 16)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "kr.co.fss.yeyak"
         minSdk = 24
         targetSdk = 36
-        versionCode = 15
-        versionName = "2.12"
+        versionCode = 16
+        versionName = "2.13"
     }
 
     buildFeatures {
