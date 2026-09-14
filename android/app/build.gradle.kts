5577162c4d96dd1e0e6f6b0ca44ea26795b2d0d5
bit4man <bit4man@gmarket.com>
2026-09-15 07:36:19 +0900

    release v2.20 (versionCode 24)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "kr.co.fss.yeyak"
         minSdk = 24
         targetSdk = 36
-        versionCode = 23
-        versionName = "2.19"
+        versionCode = 24
+        versionName = "2.20"
     }
 
     buildFeatures {
